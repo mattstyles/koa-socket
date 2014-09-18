c3b7f84649fbaf1770878fc112cbdff294027bb0
Matt Styles <matt@veryfizzyjelly.com>
2014-09-18 22:35:02 +0100

    #add - comments

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 
+/**
+ * Listeners ready to attach to a socket instance
+ */
 var listeners = [];
 
 
-
-
+/**
+ * Socket constuctor.
+ * Appends listeners to a socket instance.
+ *
+ * @param socket - socket.io socket connection
+ */
 var Socket = function( socket ) {
     listeners.forEach( function( listener ) {
         socket.on( listener.event, listener.handler );
     });
-}
+};
 
 
-
-
-
+/**
+ * Expose object to manage attaching listeners to connections
+ */
 module.exports = {
 
+    /**
+     * Stores a listener, ready to attach.
+     *
+     * @param event {String} name of event
+     * @param handler {Function} the callback to fire on event
+     */
     on: function( event, handler ) {
         listeners.push({
             event: event,
@@ -23,8 +36,12 @@ module.exports = {
         });
     },
 
-
+    /**
+     * Binds listeners to the socket connection
+     *
+     * @param socket - socket.io socket connection
+     */
     attach: function( socket ) {
         return new Socket( socket, listeners );
     }
-}
+};
