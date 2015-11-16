90b2869a05165afdcb51eaf7ac7f8409cbf84b12
Matt Styles <matt@veryfizzyjelly.com>
2015-11-16 17:31:09 +0000

    #add tests for runtime listener and middleware manipulation

---
+++ spec/stack.test.js
@@ -0,0 +1,101 @@
+
+'use strict';
+
+const tape = require( 'tape' )
+const co = require( 'co' )
+const ioc = require( 'socket.io-client' )
+const Koa = require( 'koa' )
+const IO = require( '../' )
+
+// Attaches socket.io to a server
+function connect( srv, opts ) {
+  opts = Object.assign({
+    transports: [ 'websocket' ]
+  }, opts )
+  let addr = srv.address()
+  if ( !addr ) {
+    addr = srv.listen().address()
+  }
+  let client = ioc( 'ws://0.0.0.0:' + addr.port, opts )
+  client.on( 'disconnect', () => {
+    srv.close()
+  })
+  return client
+}
+
+function application( instance ) {
+  const app = new Koa()
+  const io = instance || new IO()
+  io.attach( app )
+  return app
+}
+
+
+tape( 'Listeners can be added during runtime to connected clients', t => {
+  t.plan( 2 )
+
+  const io = new IO()
+  const app = application( io )
+
+  const client = connect( app.server )
+
+  client.on( 'connect', () => {
+    var called = false
+    client.on( 'response', ctx => {
+      called = true
+    })
+
+    client.emit( 'request' )
+
+    // Wait for a response and see if called turns true
+    setTimeout( () => {
+      t.notOk( called, 'Called should remain false' )
+
+      io.on( 'request', ctx => {
+        ctx.socket.emit( 'response' )
+      })
+
+      client.emit( 'request' )
+
+      setTimeout( () => {
+        t.ok( called, 'IO should now respond to the event and called should be true' )
+        client.disconnect()
+      }, 500 )
+    }, 500 )
+  })
+})
+
+tape( 'Middleware can be added during runtime to connected clients', t => {
+  t.plan( 2 )
+
+  const io = new IO()
+  const app = application( io )
+
+  const client = connect( app.server )
+
+  io.on( 'req1', ctx => {
+    ctx.socket.emit( 'res1', ctx.foo )
+  })
+  io.on( 'req2', ctx => {
+    ctx.socket.emit( 'res2', ctx.foo )
+  })
+
+  client.on( 'connect', () => {
+    client.on( 'res1', data => {
+      t.notOk( data, 'Middleware did not fire and attach additional prop' )
+
+      io.use( co.wrap( function *( ctx, next ) {
+        ctx.foo = 'foo'
+      }))
+
+      client.emit( 'req2' )
+    })
+
+    client.on( 'res2', data => {
+      t.ok( data, 'Middleware has fired and attached prop' )
+      client.disconnect()
+    })
+
+    client.emit( 'req1' )
+  })
+})
