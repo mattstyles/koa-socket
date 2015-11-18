698943d97861491f8063bf659522ae3b3536d380
mattstyles <matt@veryfizzyjelly.com>
2015-11-18 10:05:21 +0000

    #add coveralls

--- spec/index.js
+++ spec/index.js
@@ -3,7 +3,6 @@ const path = require( 'path' )
 const minimist = require( 'minimist' )
 
 const argv = minimist( process.argv.slice( 2 ) )
-console.log( argv )
 
 argv._.forEach( file => {
   require( path.resolve( file ) )
